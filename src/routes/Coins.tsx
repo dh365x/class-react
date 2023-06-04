3f11d2f8b6dbfbd08cc57054677b54dfd7541a2e
dh365x <dh365x@gmail.com>
2023-06-04 11:33:17 +0900

    Crypto tracker, Home part 2

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Container = styled.div`
+	margin: 0 auto;
+	max-width: 480px;
 	padding: 0px 20px;
 `;
 
@@ -18,6 +21,11 @@ const Title = styled.h1`
 `;
 
 const CoinsList = styled.ul``;
+
+const Loader = styled.span`
+	display: block;
+	text-align: center;
+`;
 
 const Coin = styled.li`
 	margin-bottom: 10px;
@@ -36,39 +44,46 @@ const Coin = styled.li`
 	}
 `;
 
-// API → https://api.coinpaprika.com/v1/coins
+interface ICoin {
+	id: string;
+	name: string;
+	symbol: string;
+	rank: number;
+	is_new: boolean;
+	is_active: boolean;
+	type: string;
+}
+
 function Coins() {
-	const coins = [
-		{
-			id: "btc-bitcoin",
-			name: "Bitcoin",
-			symbol: "BTC",
-			rank: 1,
-			is_new: false,
-			is_active: true,
-			type: "coin",
-		},
-		{
-			id: "eth-ethereum",
-			name: "Ethereum",
-			symbol: "ETH",
-			rank: 2,
-			is_new: false,
-			is_active: true,
-			type: "coin",
-		},
-	];
+	const [coins, setCoins] = useState<ICoin[]>([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch("https://api.coinpaprika.com/v1/coins");
+			const json = await response.json();
+			setCoins(json.slice(0, 20));
+			setLoading(false);
+		})();
+	}, []);
+
 	return (
 		<Container>
 			<Header>
 				<Title>코인</Title>
 			</Header>
 			<CoinsList>
-				{coins.map((coin) => (
-					<Coin key={coin.id}>
-						<Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-					</Coin>
-				))}
+				{loading ? (
+					<Loader>Loading...</Loader>
+				) : (
+					<CoinsList>
+						{coins.map((coin) => (
+							<Coin key={coin.id}>
+								<Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+							</Coin>
+						))}
+					</CoinsList>
+				)}
 			</CoinsList>
 		</Container>
 	);
