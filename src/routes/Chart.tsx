92d6a10a7a7c1600d620974a748f049f22deabd3
dh365x <dh365x@gmail.com>
2023-06-05 11:32:12 +0900

    Crypto tracker, Price chart part 1

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,4 +1,15 @@
-function Chart() {
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoinHistory } from "../api";
+
+interface ChartProps {
+	coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+	const { data, isLoading } = useQuery(["ohlcv", coinId], () =>
+		fetchCoinHistory(coinId)
+	);
+	console.log(data);
 	return <h1>Chart</h1>;
 }
 
