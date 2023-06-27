0e5b2f3c52a936e66d85b906010e8ad6590bca10
dh365x <dh365x@gmail.com>
2023-06-27 11:22:09 +0900

    StateMGMT, Forms

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,21 +1,18 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-	const [toDo, setToDo] = useState("");
-	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-		const {
-			currentTarget: { value },
-		} = event;
-		setToDo(value);
-	};
-	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		console.log(toDo);
-	};
+	const { register, watch } = useForm();
+	console.log(watch());
+
 	return (
 		<div>
-			<form onSubmit={onSubmit}>
-				<input value={toDo} onChange={onChange} placeholder="Write a to do" />
+			<form>
+				<input {...register("email")} placeholder="Email" />
+				<input {...register("firstName")} placeholder="First Name" />
+				<input {...register("lastName")} placeholder="Last Name" />
+				<input {...register("userName")} placeholder="User Name" />
+				<input {...register("password")} placeholder="Password" />
+				<input {...register("password1")} placeholder="Password1" />
 				<button>Add</button>
 			</form>
 		</div>
@@ -23,3 +20,37 @@ function ToDoList() {
 }
 
 export default ToDoList;
+
+// import React, { useState } from "react";
+
+// function ToDoList() {
+// 	const [toDo, setToDo] = useState("");
+// 	const [toDoError, setToDoError] = useState("");
+
+// 	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+// 		const {
+// 			currentTarget: { value },
+// 		} = event;
+// 		setToDoError("");
+// 		setToDo(value);
+// 	};
+// 	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+// 		event.preventDefault();
+// 		if (toDo.length < 10) {
+// 			return setToDoError("To do should be longer");
+// 		}
+// 		console.log("submit");
+// 	};
+
+// 	return (
+// 		<div>
+// 			<form onSubmit={onSubmit}>
+// 				<input value={toDo} onChange={onChange} placeholder="Write a to do" />
+// 				{toDoError !== "" ? toDoError : null}
+// 				<button>Add</button>
+// 			</form>
+// 		</div>
+// 	);
+// }
+
+// export default ToDoList;
