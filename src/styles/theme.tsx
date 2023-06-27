b79dbf81f6e8692a3f17d1b4a99b9bd1afd3cb21
dh365x <dh365x@gmail.com>
2023-06-28 00:43:42 +0900

    Trello, Styles and Placeholders

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,14 +1,8 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-	bgColor: "#2f3640",
-	textColor: "white",
-	accentColor: "#9c88ff",
-	cardBgColor: "transparent",
-};
-export const lightTheme: DefaultTheme = {
-	bgColor: "whitesmoke",
+	bgColor: "#3F8CF2",
+	boardColor: "#DADFE9",
+	cardColor: "white",
 	textColor: "black",
-	accentColor: "#9c88ff",
-	cardBgColor: "white",
 };
