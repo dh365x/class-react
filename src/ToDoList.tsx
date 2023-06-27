58919bc2414b5e815e378f84f9171f22df21529e
dh365x <dh365x@gmail.com>
2023-06-27 11:35:59 +0900

    StateMGMT, Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,18 +1,45 @@
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-	const { register, watch } = useForm();
-	console.log(watch());
+	const { register, handleSubmit, formState } = useForm();
+	const onValid = (data: any) => {
+		console.log("data:", data);
+	};
+	console.log("error:", formState.errors);
 
 	return (
 		<div>
-			<form>
-				<input {...register("email")} placeholder="Email" />
-				<input {...register("firstName")} placeholder="First Name" />
-				<input {...register("lastName")} placeholder="Last Name" />
-				<input {...register("userName")} placeholder="User Name" />
-				<input {...register("password")} placeholder="Password" />
-				<input {...register("password1")} placeholder="Password1" />
+			<form
+				style={{ display: "flex", flexDirection: "column" }}
+				onSubmit={handleSubmit(onValid)}
+			>
+				<input {...register("email", { required: true })} placeholder="Email" />
+				<input
+					{...register("firstName", { required: true })}
+					placeholder="First Name"
+				/>
+				<input
+					{...register("lastName", { required: true })}
+					placeholder="Last Name"
+				/>
+				<input
+					{...register("userName", { required: true, minLength: 2 })}
+					placeholder="User Name"
+				/>
+				<input
+					{...register("password", { required: true, minLength: 5 })}
+					placeholder="Password"
+				/>
+				<input
+					{...register("password1", {
+						required: "Password is required",
+						minLength: {
+							value: 5,
+							message: "Your password is too short.",
+						},
+					})}
+					placeholder="Password1"
+				/>
 				<button>Add</button>
 			</form>
 		</div>
@@ -20,37 +47,3 @@ function ToDoList() {
 }
 
 export default ToDoList;
-
-// import React, { useState } from "react";
-
-// function ToDoList() {
-// 	const [toDo, setToDo] = useState("");
-// 	const [toDoError, setToDoError] = useState("");
-
-// 	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-// 		const {
-// 			currentTarget: { value },
-// 		} = event;
-// 		setToDoError("");
-// 		setToDo(value);
-// 	};
-// 	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-// 		event.preventDefault();
-// 		if (toDo.length < 10) {
-// 			return setToDoError("To do should be longer");
-// 		}
-// 		console.log("submit");
-// 	};
-
-// 	return (
-// 		<div>
-// 			<form onSubmit={onSubmit}>
-// 				<input value={toDo} onChange={onChange} placeholder="Write a to do" />
-// 				{toDoError !== "" ? toDoError : null}
-// 				<button>Add</button>
-// 			</form>
-// 		</div>
-// 	);
-// }
-
-// export default ToDoList;
